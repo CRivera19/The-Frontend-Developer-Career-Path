8bd224d8e01ea7f214ce59dc4c0f59d710e002b8
ChrisRivera <ctrivera19@gmail.com>
2022-07-26 18:55:48 -0700

    added html body and css

---
+++ projects/peoplecounter/index.js
@@ -0,0 +1,18 @@
+/* Using a <background-color> */
+background: green;
+
+/* Using a <bg-image> and <repeat-style> */
+background: url("test.jpg") repeat-y;
+
+/* Using a <box> and <background-color> */
+background: border-box red;
+
+/* A single image, centered and scaled */
+background: no-repeat center/80% url("../img/image.png");
+
+/* Global values */
+background: inherit;
+background: initial;
+background: revert;
+background: revert-layer;
+background: unset;
