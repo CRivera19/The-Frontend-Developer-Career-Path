fdd95057893401eb79833cb8f63efa5933c1670c
ChrisRivera <ctrivera19@gmail.com>
2022-08-27 16:08:03 -0700

    changes before purge

--- projects/peoplecounter/index.js
+++ projects/peoplecounter/index.js
@@ -1,18 +1,30 @@
-/* Using a <background-color> */
-background: green;
+// document.getElementById("count").innerText = 0
 
-/* Using a <bg-image> and <repeat-style> */
-background: url("test.jpg") repeat-y;
+let countEl = document.getElementById("count")
+let count = 0
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+    console.log(countEl)
+}
+function decrement() {
+    count = count - 1
+    countEl.innerText = count
+    console.log(countEl)
+}
+function reset() {
+    count = 0
+    countEl.innerText = count
+    console.log(countEl)
+}
+currentTime = new Date();
+console.log(currentTime)
 
-/* Using a <box> and <background-color> */
-background: border-box red;
+let orderEl = document.getElementById("order")
+let order = "currentTime" + "countEl"
 
-/* A single image, centered and scaled */
-background: no-repeat center/80% url("../img/image.png");
-
-/* Global values */
-background: inherit;
-background: initial;
-background: revert;
-background: revert-layer;
-background: unset;
+// when click save push order to #order
+function save() {
+    let order = 
+}
+console.log(order)
